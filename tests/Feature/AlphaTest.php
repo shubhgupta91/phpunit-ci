7aee232f0be41a2cda1bd6ad5e2168ea2e4f476e
shubham gupta <shubh.gupta91@gmail.com>
2021-07-08 22:16:24 +0530

    initial commit

---
+++ tests/Feature/AlphaTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AlphaTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    // public function testExample()
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    public function testExample()
+    {
+        $response = $this->get('/alpha');
+
+        $response->assertStatus(200);
+        $response->assertSee('Alpha');
+        $response->assertDontSee('Beta');
+    }
+}
